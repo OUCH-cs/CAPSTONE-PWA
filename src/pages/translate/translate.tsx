e65a281190ab15dd110a815cd30d4789c08d1f57
김예안 <kjan0929@naver.com>
2025-03-24 20:35:14 +0900

    style: 기본 UI 구현

--- src/pages/translate/translate.tsx
+++ src/pages/translate/translate.tsx
@@ -1,3 +1,17 @@
+import RecordingIndicator from "../../components/translate/RecordingIndicator";
+import Button from "../../components/translate/Button";
+import { useState } from "react";
+
 export default function TranslatePage() {
-  return <div>translate</div>;
+  const [isTranslating, setIsTranslating] = useState<boolean>(false);
+
+  return (
+    <div className="flex flex-col items-center gap-[100px] h-screen pt-[50%]">
+      <RecordingIndicator>Recording...</RecordingIndicator>
+      <div className="flex gap-[10px]">
+        <Button disabled={isTranslating}>Start</Button>
+        <Button disabled={!isTranslating}>Finish</Button>
+      </div>
+    </div>
+  );
 }
