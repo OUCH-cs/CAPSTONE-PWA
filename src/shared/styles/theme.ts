bad5d1c2d4bc9eac4bbcb0f5e0e6ea493bc86d7c
김예안 <kjan0929@naver.com>
2025-03-27 11:31:52 +0900

    setting: 전역 theme colors 세팅

---
+++ src/shared/styles/theme.ts
@@ -0,0 +1,23 @@
+const theme = {
+  colors: {
+    primary: "#0097A7",
+    background: "#F5F9FC",
+    secondary: "#B9D6D9",
+    tertiary: "#E3F5F6",
+    red: "DC0000",
+
+    white: "#FFFFFF",
+    white_f8: "#F8F8F8",
+    white_f1: "#F1F1F5",
+    white_f5: "#F5F5F5",
+    white_e5: "#E5E5EC",
+
+    gray_de: "#DEDEDE",
+    gray_d1: "#D1D1D1",
+    gray_7: "#767676",
+    gray_4: "#434343",
+    black: "#000000",
+  },
+};
+
+export default theme;
