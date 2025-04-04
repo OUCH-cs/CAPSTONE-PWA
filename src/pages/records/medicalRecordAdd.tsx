cae351ee50789ff834c43d52b7de6a7d43470243
Hwangchanjin <slo0521@naver.com>
2025-04-04 21:11:45 +0900

    feat : medicalRecord추가페이지 구현

---
+++ src/pages/records/medicalRecordAdd.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import ArrowIcon  from "@/shared/assets/common/backarrow.svg?react";
+import { useNavigate } from "react-router-dom";
+import MedicalAddData from "@/features/records/medicalAddData";
+import StatusBar from "@/shared/assets/common/StatusBar";
+
+export default function MedicalRecordAdd() {
+    const navigate = useNavigate();
+  
+    return (
+      <div style={styles.container}>
+        <StatusBar />
+  
+        {/* 헤더 */}
+        <div style={styles.header}>
+          <button onClick={() => navigate("/records/medicalRecordList")} style={styles.backButton}>
+            <ArrowIcon width="25px" height="25px" stroke="black" style={{ marginLeft: -20 }} />
+          </button>
+          <h2 style={styles.headerTitle}>Medical Record</h2>
+        </div>
+         <MedicalAddData />
+      </div> 
+    );
+  }
+  
+  /* ✅ styles 오타 및 수정 */
+  const styles: Record<string, React.CSSProperties> = {
+    container: {
+      backgroundColor: "#F5F9FC",
+      minHeight: "100vh",
+      paddingBottom: "40px",
+      position: "relative",
+      marginTop: 14,
+      marginLeft: 16,
+      marginRight: 16,
+    },
+    backButton: {
+      position: "absolute",
+      left: 7,
+      background: "none",
+      border: "none",
+      cursor: "pointer",
+    },
+    header: {
+      marginTop: 16,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      position: "relative",
+      alignItems: "center",
+    },
+    headerTitle: {
+      fontSize: "20px",
+      fontWeight: 500,
+      color: "#000",
+      textAlign: "center",
+      marginTop: 10,
+      marginBottom: 10,
+      fontFamily: "Pretendard",
+    },
+  };
+
+  export { MedicalRecordAdd };
