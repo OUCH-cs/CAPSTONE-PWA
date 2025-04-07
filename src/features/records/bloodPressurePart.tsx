202fc4fc4f61c7d3149c5c03e7343e7544d0d173
Hwangchanjin <slo0521@naver.com>
2025-04-07 16:24:46 +0900

    feat : bloodPressure 컴포넌트 구현

---
+++ src/features/records/bloodPressurePart.tsx
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+
+export default function BloodPressurePart({ onClose, onSave }) {
+  const [contraction, setContraction] = useState("");
+  const [relaxation, setRelaxation] = useState("");
+  const [focusedIndex, setFocusedIndex] = useState<null | number>(null);
+
+  const handleSave = () => {
+    if (contraction && relaxation) {
+      onSave({ contraction, relaxation });
+      onClose();
+    }
+  };
+
+  return (
+    <div style={styles.overlay}>
+    <div style={styles.container}>
+      <div style={styles.sectionTitle}>Blood pressure</div>
+      <div style={styles.card}>
+        {/* Contraction */}
+        <div style={styles.box}>
+          <div style={styles.inputContainer}>
+            <input
+              type="number"
+              value={contraction}
+              onFocus={() => setFocusedIndex(0)}
+              onBlur={() => setFocusedIndex(null)}
+              onChange={(e) => setContraction(e.target.value)}
+              style={styles.input}
+            />
+            <label
+              style={{
+                ...styles.floatingLabelLeft
+              }}
+            >
+              Contraction
+            </label>
+          </div>
+        </div>
+
+        {/* Relaxation */}
+        <div style={styles.box}>
+          <div style={styles.inputContainer}>
+            <input
+              type="number"
+              value={relaxation}
+              onFocus={() => setFocusedIndex(1)}
+              onBlur={() => setFocusedIndex(null)}
+              onChange={(e) => setRelaxation(e.target.value)}
+              style={styles.input}
+            />
+            <label
+              style={{
+                ...styles.floatingLabelRight
+              }}
+            >
+              Relaxation
+            </label>
+          </div>
+        </div>
+      </div>
+      <h2 style = {styles.bottomText}>mmhg</h2>
+      <button style={styles.saveButton} onClick={handleSave}>Save</button>
+    </div>
+    </div>
+  );
+}
+
+const styles = {
+  overlay: {
+    position: "fixed",
+    top:0,
+    left:0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1000, // 최상위 레벨
+  },
+  container: {
+    backgroundColor: "rgba(245, 249, 252, 1)",
+    borderRadius: "20px",
+    paddingLeft: "24px",
+    width: "100%",
+    height:700,
+    marginTop:140,
+    overflowY: "auto",
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 400,
+    marginTop: 48,
+  },
+  card: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+    height: "100%",
+    // backgroundColor: "#F5F9FC",
+    padding: 10,
+    borderRadius: 10,
+    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.05)",
+  },
+  box: {
+    display: "flex",
+    marginTop: 21,
+    flexDirection: "column",
+    alignItems: "center",
+    flex: 1,
+  },
+  inputContainer: {
+    position: "relative",
+    width: "100%",
+    marginBottom: 20,
+    marginLeft:8,
+  },
+  input: {
+    width: 140,
+    padding: "18px 10px 6px 10px",
+    fontSize: 32,
+    textAlign: "center",
+    borderRadius: 10,
+    border: "1px solid #ccc",
+    outline: "none",
+    boxSizing: "border-box",
+    height:140,
+  },
+  floatingLabelLeft: {
+    position: "absolute",
+    left:35,
+    top:16,
+  },
+  floatingLabelRight: {
+    position: "absolute",
+    left:40,
+    top:16,
+  },
+  saveButton: {
+    position:"absolute",
+    marginTop: 300,
+    // marginLeft:24,
+    padding: "13px 156px",
+    backgroundColor: "#0097A7",
+    color: "white",
+    border: "none",
+    borderRadius: 5,
+    cursor: "pointer",
+  },
+  bottomText:{
+    position:"absolute",
+    top:254,
+    right: 30,
+    fontSize:16,
+    fontWeight:400,
+    color:"rgba(0,0,0,1)",
+  }
+};
