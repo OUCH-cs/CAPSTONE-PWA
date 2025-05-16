fb0a0fb8e826c3f23312a698e2e5ce6a64884783
김예안 <kjan0929@naver.com>
2025-05-16 22:22:33 +0900

    feat: 드롭다운 공통 컴포넌트 구현 [FE-51]

--- src/shared/types/components.types.ts
+++ src/shared/types/components.types.ts
@@ -1,3 +1,5 @@
+import { Department } from "@/features/search/search.types";
+
 // 버튼
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -14,4 +16,38 @@ interface IModalProps {
   toggle: () => void;
 }
 
-export type { IButtonProps, IModalProps };
+// 드롭다운
+interface IDropdown {
+  children: React.ReactNode;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface IDropdownMenu {
+  children: React.ReactNode;
+  isOpen: boolean;
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+}
+
+interface IDropdownTrigger {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+interface DepartmentFilterDropdownProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  toggle: () => void;
+  menus: Department[];
+}
+
+export type {
+  IButtonProps,
+  IModalProps,
+  IDropdown,
+  IDropdownMenu,
+  IDropdownTrigger,
+  DepartmentFilterDropdownProps,
+};
