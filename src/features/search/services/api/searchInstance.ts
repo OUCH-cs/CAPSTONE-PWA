710b67fb7600fe1e67b90f125135fc2595d16bfd
김예안 <kjan0929@naver.com>
2025-05-17 12:29:53 +0900

    setting: Google OAuth Open API Key로 변경

--- src/features/search/services/api/searchInstance.ts
+++ src/features/search/services/api/searchInstance.ts
@@ -4,8 +4,7 @@ const searchInstance = axios.create({
   baseURL: import.meta.env.VITE_NEARBY_SEARCH_URL,
   headers: {
     "Content-Type": "application/json",
-    // "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_PLACES_API_KEY,
-    "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_OPEN_API_KEY,
   },
 });
 
