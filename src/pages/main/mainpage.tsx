32b6f334b09f5379327e1c02e236fc72d6ea987d
김예안 <kjan0929@naver.com>
2025-05-17 09:56:05 +0900

    feat: 메인 병원 추천 구현 [FE-54]

--- src/pages/main/mainpage.tsx
+++ src/pages/main/mainpage.tsx
@@ -3,21 +3,43 @@ import theme from "@/shared/styles/theme";
 import HomeCamera from "@/shared/assets/home/HomeCamera.svg?react";
 import HomeGuide from "@/shared/assets/home/HomeGuide.svg?react";
 import HomeActionButton from "@/features/main/ui/HomeActionButton";
-import {HospitalList} from "@/features/main/ui/HospitalList";
 import HomeDiagnosisCard from "@/features/main/ui/DiagnosisCard";
 import MainHeader from "@/features/main/ui/MainHeader";
-
+import { fallbackLocaton } from "@/shared/consts/common";
+import { useNearbySearch } from "@/features/search/services/useNearbySearch";
+import { SearchList } from "@/features/search/ui";
+import Skeleton from "@/shared/components/skeleton/Skeleton";
 
 function MainPage() {
+  const currLocation = fallbackLocaton; // 임시 위치 설정 하드코딩
+  const { isPending, isSuccess, places } = useNearbySearch(currLocation); // 근처 병원 검색
+
   return (
     <Container>
-      <MainHeader/>
-      <HomeDiagnosisCard/>
+      <MainHeader />
+      <HomeDiagnosisCard />
       <ButtonContainer>
-        <HomeActionButton icon={<HomeCamera width={29} height={29}/>} label="Text translation" />
-        <HomeActionButton icon={<HomeGuide width={29} height={28} />} label="OUCH guide" selected />
-      </ButtonContainer>  
-      <HospitalList/>
+        <HomeActionButton
+          icon={<HomeCamera width={29} height={29} />}
+          label="Text translation"
+        />
+        <HomeActionButton
+          icon={<HomeGuide width={29} height={28} />}
+          label="OUCH guide"
+          selected
+        />
+      </ButtonContainer>
+
+      {isPending && (
+        <SkeletonList>
+          {Array.from({ length: 5 }).map((_, idx) => (
+            <Skeleton key={`notice-skeleton-${idx}`} width={236} height={152} />
+          ))}
+        </SkeletonList>
+      )}
+
+      {/* 검색결과 렌더링 */}
+      {isSuccess && <SearchList currLocation={currLocation} places={places} />}
     </Container>
   );
 }
@@ -32,7 +54,6 @@ const Container = styled.div`
   padding: 0 1rem;
 `;
 
-
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -40,5 +61,14 @@ const ButtonContainer = styled.div`
   margin-bottom: 4.3rem;
 `;
 
-
-
+const SkeletonList = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  margin-bottom: 50px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
--- src/shared/mock/index.tsx
+++ src/shared/mock/index.tsx
@@ -1,45 +1,46 @@
-export const hospitals = [
-    { id: "1", name: "Hanyang Hospital", rating: 4.3, distance: "768m", openStatus: "Open / Closed every Wednesday" },
-    { id: "2", name: "Hanyang Hospital", rating: 4.3, distance: "768m", openStatus: "Open / Closed every Wednesday" },
-    { id: "3", name: "Hanyang Hospital", rating: 4.3, distance: "768m", openStatus: "Open / Closed every Wednesday" },
-    { id: "4", name: "Hanyang Hospital", rating: 4.3, distance: "768m", openStatus: "Open / Closed every Wednesday" },
-  ];
-
 export const FAQ_DATA = [
-    {
-      id: "1",
-      question: "What is a Self-diagnosis form?",
-      answer: "A simple way to record your symptoms and health concerns in advance, making it easier to communicate with your doctor or pharmacist ",
-    },
-    {
-      id: "2",
-      question: "How it works?",
-      answer: `1. Choose your destination\n\n2. Select your symptoms\n\n3. Indicate how long you've had these symptoms\n\n4. Rate the severity of your discomfort\n\n5. Add any additional concerns you'd like to share with the healthcare provider`,
-    },
-    {
-      id: "3",
-      question: "Benefits",
-      answer: `Reduce communication barriers with your healthcare provider\n\nSave time during your visit\n\nEnsure important symptoms aren’t missed\n\nGet more accurate medical recommendations`,
-    },
-  ];
+  {
+    id: "1",
+    question: "What is a Self-diagnosis form?",
+    answer:
+      "A simple way to record your symptoms and health concerns in advance, making it easier to communicate with your doctor or pharmacist ",
+  },
+  {
+    id: "2",
+    question: "How it works?",
+    answer: `1. Choose your destination\n\n2. Select your symptoms\n\n3. Indicate how long you've had these symptoms\n\n4. Rate the severity of your discomfort\n\n5. Add any additional concerns you'd like to share with the healthcare provider`,
+  },
+  {
+    id: "3",
+    question: "Benefits",
+    answer: `Reduce communication barriers with your healthcare provider\n\nSave time during your visit\n\nEnsure important symptoms aren’t missed\n\nGet more accurate medical recommendations`,
+  },
+];
 
-  export const SYMPTOMS = ['Sore throat', 'Chills', 'Runny nose', 'Cough','감기','기침','감기 몸살'];
+export const SYMPTOMS = [
+  "Sore throat",
+  "Chills",
+  "Runny nose",
+  "Cough",
+  "감기",
+  "기침",
+  "감기 몸살",
+];
 
+export const DURATION_OPTIONS = [
+  "LESS_THAN_DAY",
+  "ONE_TO_THREE_DAYS",
+  "MORE_THAN_THREE_DAYS",
+  "MORE_THAN_WEEK",
+  "MORE_THAN_MONTH",
+];
 
-  export const DURATION_OPTIONS = [
-    "LESS_THAN_DAY",
-    "ONE_TO_THREE_DAYS",
-    "MORE_THAN_THREE_DAYS",
-    "MORE_THAN_WEEK",
-    "MORE_THAN_MONTH",
-  ];
+export const DURATION_LABELS: Record<string, string> = {
+  LESS_THAN_DAY: "Less than a day",
+  ONE_TO_THREE_DAYS: "1-3 days",
+  MORE_THAN_THREE_DAYS: "More than 3 days",
+  MORE_THAN_WEEK: "More than a week",
+  MORE_THAN_MONTH: "More than a month",
+};
 
-  export const DURATION_LABELS: Record<string, string> = {
-    LESS_THAN_DAY: "Less than a day",
-    ONE_TO_THREE_DAYS: "1-3 days",
-    MORE_THAN_THREE_DAYS: "More than 3 days",
-    MORE_THAN_WEEK: "More than a week",
-    MORE_THAN_MONTH: "More than a month",
-  };
-  
-  export const ITEMS_PER_ROW = 5;
+export const ITEMS_PER_ROW = 5;
