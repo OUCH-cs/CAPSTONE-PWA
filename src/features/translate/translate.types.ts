69f58f3940db2a1ea3fb6256a3df2430be084661
Hwangchanjin <slo0521@naver.com>
2025-05-20 16:42:15 +0900

    chore : 누락파일 추가

---
+++ src/features/translate/translate.types.ts
@@ -0,0 +1,39 @@
+import { ClassPostProps } from "../diagnosis/ui/Funnel";
+
+interface IRecordingIndicatorProps {
+  children: React.ReactNode;
+}
+
+interface GuideFunnelProps extends Omit<ClassPostProps, "nextClickHandler"> {
+  setStep: React.Dispatch<React.SetStateAction<string>>;
+  currentStep: string;
+}
+
+type Locale = "ko" | "en";
+type LocalizedText = Record<Locale, string>;
+
+interface Guide {
+  step: number;
+  title: LocalizedText;
+  icon: string;
+  purpose: LocalizedText;
+  whatToExpect: LocalizedText[];
+  keyPhrases: LocalizedText[];
+  actionGuide: LocalizedText[];
+  possibleQuestions: LocalizedText[] | null;
+  tips: LocalizedText[];
+}
+
+interface GuideAccordionProps {
+  title: string;
+  text: LocalizedText[];
+}
+
+export type {
+  IRecordingIndicatorProps,
+  GuideFunnelProps,
+  Locale,
+  LocalizedText,
+  Guide,
+  GuideAccordionProps,
+};
