42ee16b3338fa3535f73c0c1d06bb6fd6bc4b2ad
Hwangchanjin <slo0521@naver.com>
2025-05-20 19:16:56 +0900

    chore : navigate대신 link태그 이용

--- src/features/mypage/ui/EditHeader.tsx
+++ src/features/mypage/ui/EditHeader.tsx
@@ -1,22 +1,19 @@
 import styled from "@emotion/styled";
 import ArrowIcon from "@/shared/assets/common/backarrow.svg?react";
-import { useNavigate} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const EditHeader = () => {
-     const navigate = useNavigate();
-
   return (
     <Header>
-      <BackButton onClick={() => navigate("/mypage")}>
-          <ArrowIcon width="25px" height="25px" stroke="black" />
-        </BackButton>
-        <TitleText>Edit Profile</TitleText>
+      <BackLink to="/mypage">
+        <ArrowIcon width="25px" height="25px" stroke="black" />
+      </BackLink>
+      <TitleText>Edit Profile</TitleText>
     </Header>
   );
 };
 
 export default EditHeader;
-
 
 const Header = styled.div`
   margin-top: 26px;
@@ -25,22 +22,23 @@ const Header = styled.div`
   align-items: center;
   padding: 16px 16px;
   margin-bottom: 40px;
-  position: relative; 
-
+  position: relative;
 `;
 
 const TitleText = styled.p`
   font-size: 20px;
   font-weight: 500;
-  white-space: nowrap; 
-  position: absolute;  /* TitleText를 중앙에 위치시킴 */
+  white-space: nowrap;
+  position: absolute; /* TitleText를 중앙에 위치시킴 */
 `;
 
-const BackButton = styled.button`
+const BackLink = styled(Link)`
+  position: absolute; /* ArrowIcon을 왼쪽으로 고정 */
+  left: 16px;
   background: none;
   border: none;
   cursor: pointer;
-   position: absolute;  /* ArrowIcon을 왼쪽으로 고정 */
-  left: 16px;  /* 왼쪽에서 16px 떨어지게 배치 */
+  display: flex;
+  align-items: center;
+  text-decoration: none;
 `;
-
