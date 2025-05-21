b1743cfe2f84f4d66b92cdd2cac273292dbf600b
김예안 <kjan0929@naver.com>
2025-05-21 23:11:08 +0900

    setting: netlify redirects 설정

--- src/features/search/services/api/searchInstance.ts
+++ src/features/search/services/api/searchInstance.ts
@@ -4,7 +4,8 @@ const searchInstance = axios.create({
   baseURL: import.meta.env.VITE_NEARBY_SEARCH_URL,
   headers: {
     "Content-Type": "application/json",
-    "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    // "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    "X-Goog-Api-Key": "AIzaSyBcT70j2lyUM80wX1S8jTCPFCNV4MXJlMU",
   },
 });
 
